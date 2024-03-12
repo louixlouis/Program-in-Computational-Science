5bdc65f1e08d8aa7f2214274abdf070445460f37
louixlouis <shinyk92@gmail.com>
2024-03-12 14:56:21 +0900

    Upload HW_2.

---
+++ HW/HW_2/HW_2.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#%%
+"""
+Problem 1.
+
+When two strings, "message" and "text", are provided, 
+determine how many times "message" occurs as a substring of "text". 
+It's important to note that occurrences of "message" may overlap in "text"
+For instance, the message "aa" occurs 3 times in the text "aaaabb".
+
+The following conditions must be satified.
+    1. If the length of "message" is longer than the length of "text", 
+       the occurences of "message" will be -1.
+    2. If either "message" or "text" has a length of 0, the occurrences will also be -1.
+"""
+# TODO.
+message = 'ss'
+text = 'ssssissisasaasssasass'
+occurrences  = 0
+
+
+
+
+
+
+#%%
+#############################
+# Code to check the answer.
+# Do not modify below code.
+#############################
+if occurrences == 7:
+    print('Correct.')
+else:
+    print('Wrong.')
+
+
+
+
+
