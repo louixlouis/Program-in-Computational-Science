6669f38905acc47903606f8238b6dcc444c1e5c9
louixlouis <shinyk92@gmail.com>
2024-03-19 17:15:15 +0900

    .

---
+++ HW/HW_5/HW_5.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+#%%
+"""
+Problem 1.
+
+Create a function that find the max value. 
+
+
+"""
+
+def find_max(numbers):
+    # TODO.
+
+
+#############################
+# Test cases.
+# Do not modify below code.
+#############################
+
+
+#%%
+"""
+Problem 2.
+
+Create a function that returns descending ordered number list. 
+
+  
+"""
+def descending(numbers):
+    # TODO.
+
+
+#############################
+# Test cases.
+# Do not modify below code.
+#############################
+
+
+
+#%%
+"""
+Problem 3.
+
+Create a function calculate matrix multiplication.
+
+
+"""
+
+def mat_mul(x, y):
+    # TODO.
+
+#############################
+# Test cases.
+# Do not modify below code.
+#############################
+
+#%%
+"""
+Problem 4.
+
+Codes..
+name, score, ...
+
+
+"""
+
+def ranking(x, y):
+    # TODO.
+
+#############################
+# Test cases.
+# Do not modify below code.
+#############################
