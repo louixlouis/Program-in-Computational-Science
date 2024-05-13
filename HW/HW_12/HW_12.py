8581c5689992a8a0a49808a0056728f863f76aee
louixlouis <shinyk92@gmail.com>
2024-05-13 17:44:20 +0900

    .

---
+++ HW/HW_12/HW_12.py
@@ -0,0 +1,21 @@
+#%%
+"""
+Problem 1.
+"""
+
+class Sorting:
+    def __init__(self, numbers):
+        self.numbers = numbers
+    
+    def print_func(self):
+        pass
+
+    def selection_sort(self, reverse=False):
+        pass
+
+    def bubble_sort(self, reverse=False):
+        pass
+
+    def merge_sort(self, numbers, reverse=False):
+        pass
+
